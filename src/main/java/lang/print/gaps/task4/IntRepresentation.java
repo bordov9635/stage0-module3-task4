6d56f6ce4934181633545fac560d405e4f138779
Siarhei <bordov9635@gmail.com>
2023-04-15 18:05:42 +0300

    commit1

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,17 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int c = 99;
+        int h = 104;
+        int a = 97;
+        int r = 114;
+        char charC = (char)c;
+        char charH = (char)h;
+        char charA = (char)a;
+        char charR = (char)r;
+        System.out.println(charC);
+        System.out.println(charH);
+        System.out.println(charA);
+        System.out.println(charR);
     }
 }
